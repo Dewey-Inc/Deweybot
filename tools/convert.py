1bf5c316ca25be3d1d57fff017ef21acacc6292c
Jase Williams <jased@jased.xyz>
2026-06-18 03:19:15 -0400

    ok

--- tools/convert.py
+++ tools/convert.py
@@ -54,6 +54,7 @@ else:
 
 
 total_start = time.time()
+failures = []
 
 for file in files:
     filename = file.split("/")[-1].split(".")[0]
@@ -61,37 +62,41 @@ for file in files:
 
     start = time.time()
 
-    img = Image.open(file)
-    small = []
-    inv_frames = []
-    inv_small = []
-    durations = []
+    try:
+        img = Image.open(file)
+        small = []
+        inv_frames = []
+        inv_small = []
+        durations = []
 
-    for frame in ImageSequence.Iterator(img):
-        small.append(ImageOps.contain(frame, (350, 500)))
-        inv_frames.append(ImageOps.invert(frame.convert("RGB")))
-        inv_small.append(ImageOps.contain(inv_frames[-1], (350, 500)))
-        durations.append(frame.info.get("duration", 40))
-    
-    ext = "png"
-    if len(small) > 1:
-        ext = "gif"
-        small[0].save(
-            f"{path}/small/{filename}.{ext}",format="GIF",save_all=True,append_images=small[1:],loop=0,durations=durations,disposal=2
-        )
-        inv_frames[0].save(
-            f"{path}/E{filename}.{ext}",format="GIF",save_all=True,append_images=inv_frames[1:],loop=0,durations=durations
-        )
-        inv_small[0].save(
-            f"{path}/small/E{filename}.{ext}",format="GIF",save_all=True,append_images=inv_small[1:],loop=0,durations=durations
-        )
-    else:
-        small[0].save(f"{path}/small/{filename}.{ext}", format="png")
-        inv_frames[0].save(f"{path}/E{filename}.{ext}", format="png")
-        inv_small[0].save(f"{path}/small/E{filename}.{ext}", format="png")
-    filename += f".{ext}"
-
-    print(f"took {round(number=time.time()-start,ndigits=2)}s")
+        for frame in ImageSequence.Iterator(img):
+            small.append(ImageOps.contain(frame, (350, 500)))
+            inv_frames.append(ImageOps.invert(frame.convert("RGB")))
+            inv_small.append(ImageOps.contain(inv_frames[-1], (350, 500)))
+            durations.append(frame.info.get("duration", 40))
+        
+        ext = "png"
+        if len(small) > 1:
+            ext = "gif"
+            small[0].save(
+                f"{path}/small/{filename}.{ext}",format="GIF",save_all=True,append_images=small[1:],loop=0,durations=durations,disposal=2
+            )
+            inv_frames[0].save(
+                f"{path}/E{filename}.{ext}",format="GIF",save_all=True,append_images=inv_frames[1:],loop=0,durations=durations
+            )
+            inv_small[0].save(
+                f"{path}/small/E{filename}.{ext}",format="GIF",save_all=True,append_images=inv_small[1:],loop=0,durations=durations
+            )
+        else:
+            small[0].save(f"{path}/small/{filename}.{ext}", format="png")
+            inv_frames[0].save(f"{path}/E{filename}.{ext}", format="png")
+            inv_small[0].save(f"{path}/small/E{filename}.{ext}", format="png")
+        filename += f".{ext}"
+    except Exception as e:
+        failures.append((file, e))
+        print("\x1b[31mfailed... \x1b[0m", end="") # i want it to be red.... to stick out....
+    finally:
+        print(f"took {round(number=time.time()-start,ndigits=2)}s")
 
 
 print("Done!")
